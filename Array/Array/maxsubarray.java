432b33f8c83df614624ff7a4cc436c50377553fe
HardikSingh2801 <hardiksingh2002286@gmail.com>
2023-05-04 21:39:37 +0530

    not much

--- Array/Array/maxsubarray.java
+++ Array/Array/maxsubarray.java
@@ -1,26 +1,22 @@
-package Array;
-
 public class maxsubarray {
+
+    public static int maxofsub(int arr[]){
+
+        int maxsf= arr[0];
+        int maxed = arr[0];
+
+        for(int i=1;i<arr.length;i++){
+          maxed= Math.max(maxed + arr[i], i);
+          maxsf= Math.max(maxsf,maxed);
+        }
+        return maxsf;
+    }
+
     public static void main(String[] args) {
+        int[] arr= {3,4,2,3,-3,2,3,5};
 
-      int arr[] = {5,6,3,-6,9,11,7};
-
-      int max=0;
-
-      int sum=0;
-
-      for(int i=0;i<arr.length-1;i++){
-        if(arr[i]<0){
-            sum=0;
-            i++;
-        }
-
-        sum+=arr[i];
-        if(max<sum){
-            max=sum;
-        }
-      }
-      System.out.println(max);
+        System.out.println(maxofsub(arr));
+        
     }
     
 }
