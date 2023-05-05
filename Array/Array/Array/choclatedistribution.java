9bcd82c0d058cf31e34bcbd622274b865d97ec47
HardikSingh2801 <hardiksingh2002286@gmail.com>
2023-05-05 13:10:30 +0530

    choclate distribtution

---
+++ Array/Array/Array/choclatedistribution.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class choclatedistribution {
+
+    public static void mindiff(int arr[],int n,int m) {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
